7879b0e8053f8c118b3983513071ece86aef85a3
Diego Vilhalva <diegorocha450@yahoo.com>
2025-04-08 13:16:32 -0300

    installing packages and created inital context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-teal-400 text-4xl text-white h-screen flex justify-center items-center'>
+      <div className="border w-[400px] h-[400px] flex items-center justify-center rounded-xl">
+        <h1>App</h1>
+      </div>
+    </div>
   )
 }
 
