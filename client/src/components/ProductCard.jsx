db86cd570a53e645e896f931cefa284a49d8b4a2
Diego Vilhalva <diegorocha450@yahoo.com>
2025-04-10 12:39:19 -0300

    product page category created

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+
 import { assets } from '../assets/assets';
 import { useAppContext } from '../context/AppContext';
 
-const ProductCard = ({product}) => {
-    const [count, setCount] = useState(0);
-    const {currency,addToCart,removeFromCart,cartItems} = useAppContext()
- 
+const ProductCard = ({ product }) => {
+
+    const { currency, addToCart, removeFromCart, cartItems, navigate } = useAppContext()
+
     return product && (
-        <div className="border border-gray-500/20 rounded-md px-3 md:px-4 py-2 bg-white w-full transition-transform duration-200 hover:-translate-y-1 hover:shadow-lg">
+        <div className="border border-gray-500/20 rounded-md px-3 md:px-4 py-2 bg-white w-full transition-transform duration-200 hover:-translate-y-1 hover:shadow-lg" onClick={() => { navigate(`/products/${product.category.toLowerCase()}/${product._id}`); scrollTo(0, 0) }}>
             <div className="group cursor-pointer flex items-center justify-center px-2">
                 <img className="group-hover:scale-105 transition max-w-26 md:max-w-36" src={product.image[0]} alt={product.name} />
             </div>
@@ -17,8 +17,8 @@ const ProductCard = ({product}) => {
                 <div className="flex items-center gap-0.5">
                     {Array(5).fill('').map((_, i) => (
                         (
-                           <img key={i} src={i < 4 ? assets.star_icon : assets.star_dull_icon} alt='star' className='md:w-3.5 w-3' />
-                        ) 
+                            <img key={i} src={i < 4 ? assets.star_icon : assets.star_dull_icon} alt='star' className='md:w-3.5 w-3' />
+                        )
                     ))}
                     <p>(4)</p>
                 </div>
@@ -26,19 +26,19 @@ const ProductCard = ({product}) => {
                     <p className="md:text-xl text-base font-medium text-primary">
                         {currency} ${product.offerPrice}{" "} <span className="text-gray-500/60 md:text-sm text-xs line-through">{currency}${product.price}{" "}</span>
                     </p>
-                    <div className="text-primary" onClick={(e) => {e.stopPropagation()}}>
-                        {  !cartItems[product._id] ? (
+                    <div className="text-primary" onClick={(e) => { e.stopPropagation() }}>
+                        {!cartItems[product._id] ? (
                             <button className="flex items-center justify-center gap-1 bg-primary/10 border border-primary/40 md:w-[80px] w-[64px] h-[34px] rounded cursor-pointer " onClick={() => addToCart(product._id)} >
-                               <img src={assets.cart_icon} alt="cart icon" />
+                                <img src={assets.cart_icon} alt="cart icon" />
                                 Add
                             </button>
                         ) : (
                             <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-primary/25 rounded select-none">
-                                <button onClick={() => {removeFromCart(product._id)}} className="cursor-pointer text-md px-3 py-1 h-full" >
+                                <button onClick={() => { removeFromCart(product._id) }} className="cursor-pointer text-md px-3 py-1 h-full" >
                                     -
                                 </button>
                                 <span className="w-5 text-center">{cartItems[product._id]}</span>
-                                <button onClick={() => {addToCart(product._id)}} className="cursor-pointer text-md px-3 py-1 h-full" >
+                                <button onClick={() => { addToCart(product._id) }} className="cursor-pointer text-md px-3 py-1 h-full" >
                                     +
                                 </button>
                             </div>
