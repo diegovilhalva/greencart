cecece99e5d067b77b39f15674a4d144109a8ac9
Diego Vilhalva <diegorocha450@yahoo.com>
2025-04-15 09:49:58 -0300

    backend setup

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import connectDB from "./configs/db.js"
+
+
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 4000
+const allowedOrigins = ['http://localhost:5173/']
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: allowedOrigins, credentials: true }))
+
+
+app.get("/", (req, res) => {
+    res.send("ok")
+})
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`🚀 Server is running on ${PORT}`);
+    });
+}).catch((err) => {
+    console.error("❌ Connetion error:", err);
+});
