6b7bf3375cd7df9a538ff766ad96d3d8c8019646
Diego Vilhalva <diegorocha450@yahoo.com>
2025-04-16 09:27:47 -0300

    logion functionality added

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,14 +1,14 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
-import { validateRegisterInput } from "../helpers/validators.js"
+import { validateLoginInput, validateRegisterInput } from "../helpers/validators.js"
 
 
 export const register = async (req, res) => {
     try {
         const { name, email, password } = req.body
         const errorMsg = validateRegisterInput({ name, email, password });
-        
+
         if (errorMsg) return res.status(400).json({ success: false, message: errorMsg });
 
 
@@ -51,3 +51,44 @@ export const register = async (req, res) => {
         res.status(500).json({ success: false, message: error.message })
     }
 }
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const errorMsg = validateLoginInput({ email, password });
+        if (errorMsg) return res.status(400).json({ success: false, message: errorMsg });
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ success: false, message: "Invalid credentials" });
+        }
+
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ success: false, message: "Invalid credentials" });
+        }
+
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+
+        const userWithoutPassword = user.toObject();
+        delete userWithoutPassword.password;
+
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: process.env.NODE_ENV !== "development" ? "none" : "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        return res.status(200).json({ success: true, user: userWithoutPassword });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+};
--- server/helpers/validators.js
+++ server/helpers/validators.js
@@ -7,4 +7,11 @@ export const validateRegisterInput = ({ name, email, password }) => {
     if (name.trim().length < 2) return "Name must be at least 2 characters";
     return null;
 };
-  
+
+
+export const validateLoginInput = ({ email, password }) => {
+    if (!email || !password) return "Email and password are required";
+    if (!validator.isEmail(email)) return "Invalid email format";
+    if (password.length < 6) return "Password must be at least 6 characters";
+    return null;
+}
